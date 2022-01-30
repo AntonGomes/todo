d1133d7b46f9faccae3460beae4e0cb8366c56fe
antongomes <aomlgomes@gmail.com>
2022-01-30 17:09:08 +0000

    initial commit

---
+++ ToDo.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+#include <string>
+
+using namespace std;
+string getTask(){
+        string task;
+        cout << "Enter new task:\n";
+        cint >> task;
+        return task;
+    }
+
+bool updateNeeded(){
+    bool update;
+    cout << "Do you need to update ToDo?";
+    cint >> update;
+    return update;
+}
+
+
+int main(){
+    int i = 0
+    string toDos[10];
+    while updateNeeded() == true {
+        toDos[i]=getTask();
+        i++;
+    }
+    cout << "Good Day.";
+    return 0;
+}
+
